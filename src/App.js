8afb1ef27ffa6bc7c70d38ccf0da92fbe6da3056
KumarOjas <ojaskumargupta@gmail.com>
2024-10-02 06:23:47 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Calculator from './Calculator';
+import './App.css'; // Optional: for any global styles
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>React Calculator</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
+
+
